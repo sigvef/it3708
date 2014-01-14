d2a25942110dbcc1d3999f8d87bc3654524e6a44
Emil Grønnbeck <emilgroenn@gmail.com>
2014-01-14 18:48:53 +0100

    added init -> basic_setup

--- assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
+++ assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
@@ -5,6 +5,16 @@ from controller import *   # controller comes with Webots
 
 class BeardedOctoNinja(DifferentialWheels):
 
+	def basic_setup(self, tempo = 1.0):
+    	self.timestep = int(self.getBasicTimeStep()) # Fetched from WorldInfo.basicTimeStep (in the Webots world)
+    	self.tempo = tempo
+    	self.enableEncoders(self.timestep)
+    	self.camera = self.getCamera('camera')
+    	self.camera.enable(4*self.timestep)
+    	print "Camera width: " , self.camera.getWidth()
+    	self.dist_sensor_values = [0 for i in range(self.num_dist_sensors)]
+    	self.dist_sensors = [self.getDistanceSensor('ps'+str(x)) for x in range(self.num_dist_sensors)]  # distance sensors
+    	map((lambda s: s.enable(self.timestep)), self.dist_sensors) # Enable all distance sensors
 
 
     def set_speed(self, speed):
@@ -22,6 +32,8 @@ class BeardedOctoNinja(DifferentialWheels):
     def set_rotation(self, rotations_speed):
         """ passing in rotationspeed and calculates speed on left/ and right wheel """
         pass
+  
+
 
     #
     # Sensors and Camera
@@ -45,3 +57,4 @@ class BeardedOctoNinja(DifferentialWheels):
         strImage=self.camera.getImage()
         im = Image.fromstring('RGB',(self.camera.getWidth(), self.camera.getHeight()), strImage)
         return im
+
