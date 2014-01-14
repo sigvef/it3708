aa657c1c94b813b8e18957a9b7008b53da93a5a6
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-01-14 18:46:43 +0100

    add sensors and camera functions

--- assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
+++ assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
@@ -5,6 +5,8 @@ from controller import *   # controller comes with Webots
 
 class BeardedOctoNinja(DifferentialWheels):
 
+
+
     def set_speed(self, speed):
         """moving either forward or backwards"""
         pass
@@ -21,4 +23,25 @@ class BeardedOctoNinja(DifferentialWheels):
         """ passing in rotationspeed and calculates speed on left/ and right wheel """
         pass
 
-    
+    #
+    # Sensors and Camera
+    #
+
+    # Returns proximities
+    def get_proximities(self):
+        for i in range(self.num_dist_sensors):
+            self.dist_sensor_values[i] = self.dist_sensors[i].getValue()
+            return self.dist_sensor_values
+
+    # Returns parsed image
+    def snapshot(self, show = False):
+        im = self.get_image()
+        if show: 
+            im.show()
+        return im
+
+    # Delegate method
+    def get_image(self):
+        strImage=self.camera.getImage()
+        im = Image.fromstring('RGB',(self.camera.getWidth(), self.camera.getHeight()), strImage)
+        return im
