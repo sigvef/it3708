f42e1bf747da722e9ee8f0125372dc71dddb3841
Emil Grønnbeck <emilgroenn@gmail.com>
2014-01-14 18:31:07 +0100

    semi-specced bearded_octoninja

--- assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded_octoninja.py
+++ assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded_octoninja.py
@@ -1,14 +1,25 @@
 from controller import *   # controller comes with Webots
 
+
+"""avoid obstacles"""
+
 class BeardedOctoNinja(DifferentialWheels):
 
+    def set_speed(self, speed):
+        """moving either forward or backwards"""
+        pass
+    
+    def accelerate(self, acceleration):
+        """increment speed"""
+        pass
 
+    def stop(self):
+        """stops the epuck"""
+        pass 
 
+    def set_rotation(self, rotations_speed):
+        """ passing in rotationspeed and calculates speed on left/ and right wheel """
+        pass
 
-
-
-
-
-
-
+    
 
