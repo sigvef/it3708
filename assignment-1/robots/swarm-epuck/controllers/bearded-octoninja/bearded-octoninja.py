28226861f940ead73a5bf55cdeeef3d3e45a4cc8
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-01-15 18:44:26 +0100

    Enforce Flake8 style

--- assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
+++ assignment-1/robots/swarm-epuck/controllers/bearded-octoninja/bearded-octoninja.py
@@ -1,28 +1,26 @@
-from controller import *   # controller comes with Webots
-import time                # A Python primitive module
-import math                #   "            "
-import Image               # An extra Python module (that you'll have to download)
-import imagepro            # A module provided by Keith Downing for this assignment
+from controller import DifferentialWheels
+import Image
 from behaviours import wander, avoid_objects
+
 
 class BeardedOctoNinja(DifferentialWheels):
 
     num_dist_sensors = 8
 
-    def basic_setup(self, tempo = 1.0):
-        self.timestep = int(self.getBasicTimeStep()) # Fetched from WorldInfo.basicTimeStep (in the Webots world)
+    def basic_setup(self, tempo=1.0):
+        self.timestep = int(self.getBasicTimeStep())
         self.tempo = tempo
         self.enableEncoders(self.timestep)
         self.camera = self.getCamera('camera')
         self.camera.enable(4*self.timestep)
-        print "Camera width: " , self.camera.getWidth()
+        print "Camera width: ", self.camera.getWidth()
 
         self.dist_sensor_values = [0 for i in range(self.num_dist_sensors)]
-        self.dist_sensors = [self.getDistanceSensor('ps'+str(x)) for x in range(self.num_dist_sensors)]  # distance sensors
-        map((lambda s: s.enable(self.timestep)), self.dist_sensors) # Enable all distance sensors
+        self.dist_sensors = [self.getDistanceSensor('ps'+str(x))
+                             for x in range(self.num_dist_sensors)]
+        map((lambda s: s.enable(self.timestep)), self.dist_sensors)
         self.speed = 0.0
         self.rotation_speed = 0.0
-
 
     #
     # Movement
@@ -49,7 +47,7 @@ class BeardedOctoNinja(DifferentialWheels):
     def set_rotation_speed(self, rotation_speed):
         self.rotation_speed = rotation_speed
         self._update_wheel_speeds()
-    
+
     def accelerate(self, acceleration):
         self.set_speed(self.speed + acceleration)
 
@@ -66,16 +64,17 @@ class BeardedOctoNinja(DifferentialWheels):
         return [sensor.getValue() for sensor in self.dist_sensors]
 
     # Returns parsed image
-    def snapshot(self, show = False):
+    def snapshot(self, show=False):
         im = self.get_image()
-        if show: 
+        if show:
             im.show()
         return im
 
     # Delegate method
     def get_image(self):
-        strImage=self.camera.getImage()
-        im = Image.fromstring('RGB',(self.camera.getWidth(), self.camera.getHeight()), strImage)
+        strImage = self.camera.getImage()
+        im = Image.fromstring(
+            'RGB', (self.camera.getWidth(), self.camera.getHeight()), strImage)
         return im
 
     def get_sensors(self):
